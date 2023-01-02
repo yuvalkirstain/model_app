bbc0171ae97b7fc1606399cb5c319c30de363803
yuvalkirstain <kirstain.yuval@gmail.com>
2023-01-02 16:33:02 +0200

    use two processes per TPU

--- startup.sh
+++ startup.sh
@@ -20,16 +20,10 @@ tmux kill-server
 export TPU_CHIPS_PER_HOST_BOUNDS="1,2,1"
 export TPU_HOST_BOUNDS="1,1,1"
 
-export TPU_VISIBLE_DEVICES="0,1"
-export TPU_MESH_CONTROLLER_ADDRESS="localhost:8476"
-export TPU_MESH_CONTROLLER_PORT="8476"
-tmux new-session -d -s model_app_start 'python3 -m gunicorn -t 300 -b :5000 -w 1 -k uvicorn.workers.UvicornWorker api:app'
+tmux new-session -d -s model_app_start 'export TPU_VISIBLE_DEVICES="0,1"; export TPU_MESH_CONTROLLER_ADDRESS="localhost:8476"; export TPU_MESH_CONTROLLER_PORT="8476"; python3 -m gunicorn -t 300 -b :5000 -w 1 -k uvicorn.workers.UvicornWorker api:app'
 
 sleep 30
 
-export TPU_VISIBLE_DEVICES="2,3"
-export TPU_MESH_CONTROLLER_ADDRESS="localhost:8477"
-export TPU_MESH_CONTROLLER_PORT="8477"
-tmux new-session -d -s model_app_end 'python3 -m gunicorn -t 300 -b :5001 -w 1 -k uvicorn.workers.UvicornWorker api:app'
+tmux new-session -d -s model_app_end 'export TPU_VISIBLE_DEVICES="2,3"; export TPU_MESH_CONTROLLER_ADDRESS="localhost:8477"; export TPU_MESH_CONTROLLER_PORT="8477"; python3 -m gunicorn -t 300 -b :5001 -w 1 -k uvicorn.workers.UvicornWorker api:app'
 
 tmux ls
