bae2890980775d323a67453180ff9c952a28561b
yuvalkirstain <kirstain.yuval@gmail.com>
2023-01-02 16:28:49 +0200

    use two processes per TPU

--- startup.sh
+++ startup.sh
@@ -16,5 +16,18 @@ git pull
 echo "Starting service"
 fuser -k 5000/tcp
 tmux kill-server
-tmux new-session -d -s model_app 'python3 -m gunicorn -t 300 -b :5000 -w 1 -k uvicorn.workers.UvicornWorker api:app'
+
+export TPU_CHIPS_PER_HOST_BOUNDS="1,2,1"
+export TPU_HOST_BOUNDS="1,1,1"
+
+export TPU_VISIBLE_DEVICES="0,1"
+export TPU_MESH_CONTROLLER_ADDRESS="localhost:8476"
+export TPU_MESH_CONTROLLER_PORT="8476"
+tmux new-session -d -s model_app_start 'python3 -m gunicorn -t 300 -b :5000 -w 1 -k uvicorn.workers.UvicornWorker api:app'
+
+export TPU_VISIBLE_DEVICES="2,3"
+export TPU_MESH_CONTROLLER_ADDRESS="localhost:8477"
+export TPU_MESH_CONTROLLER_PORT="8477"
+tmux new-session -d -s model_app_end 'python3 -m gunicorn -t 300 -b :5001 -w 1 -k uvicorn.workers.UvicornWorker api:app'
+
 tmux ls
