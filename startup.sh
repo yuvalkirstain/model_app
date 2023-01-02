97b0b34c0d0e3dd9e64d4ae94e59486f3e543f80
yuvalkirstain <kirstain.yuval@gmail.com>
2023-01-02 16:30:51 +0200

    use two processes per TPU

--- startup.sh
+++ startup.sh
@@ -25,6 +25,8 @@ export TPU_MESH_CONTROLLER_ADDRESS="localhost:8476"
 export TPU_MESH_CONTROLLER_PORT="8476"
 tmux new-session -d -s model_app_start 'python3 -m gunicorn -t 300 -b :5000 -w 1 -k uvicorn.workers.UvicornWorker api:app'
 
+sleep 30
+
 export TPU_VISIBLE_DEVICES="2,3"
 export TPU_MESH_CONTROLLER_ADDRESS="localhost:8477"
 export TPU_MESH_CONTROLLER_PORT="8477"
