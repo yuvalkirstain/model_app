1d3583d23ccc3fa61f6ebfdd26d846b13d246663
yuvalkirstain <kirstain.yuval@gmail.com>
2023-02-12 19:29:26 +0200

    small fix - name of model

--- startup.sh
+++ startup.sh
@@ -23,11 +23,11 @@ tmux kill-server
 
 
 tmux new-session -d -s model_app1 'export MODEL_ID="yuvalkirstain/dreamlike-photoreal-2-flax"; export TPU_HOST_BOUNDS="1,1,1"; export TPU_CHIPS_PER_HOST_BOUNDS="1,2,1"; export TPU_VISIBLE_DEVICES="0"; export TPU_MESH_CONTROLLER_ADDRESS="localhost:8476"; export TPU_MESH_CONTROLLER_PORT="8476"; python3 -m gunicorn -t 300 -b :5000 -w 1 -k uvicorn.workers.UvicornWorker api:app'
-tmux new-session -d -s model_app2 'export MODEL_ID="yuvalkirstain/stable-diffusion-2-1"; export TPU_HOST_BOUNDS="1,1,1"; export TPU_CHIPS_PER_HOST_BOUNDS="1,2,1"; export TPU_VISIBLE_DEVICES="1"; export TPU_MESH_CONTROLLER_ADDRESS="localhost:8477"; export TPU_MESH_CONTROLLER_PORT="8477"; python3 -m gunicorn -t 300 -b :5001 -w 1 -k uvicorn.workers.UvicornWorker api:app'
+tmux new-session -d -s model_app2 'export MODEL_ID="stabilityai/stable-diffusion-2-1"; export TPU_HOST_BOUNDS="1,1,1"; export TPU_CHIPS_PER_HOST_BOUNDS="1,2,1"; export TPU_VISIBLE_DEVICES="1"; export TPU_MESH_CONTROLLER_ADDRESS="localhost:8477"; export TPU_MESH_CONTROLLER_PORT="8477"; python3 -m gunicorn -t 300 -b :5001 -w 1 -k uvicorn.workers.UvicornWorker api:app'
 
 sleep 30
 
-tmux new-session -d -s model_app3 'export MODEL_ID="stabilityai/dreamlike-photoreal-2-flax"; export TPU_HOST_BOUNDS="1,1,1"; export TPU_CHIPS_PER_HOST_BOUNDS="1,2,1"; export TPU_VISIBLE_DEVICES="2"; export TPU_MESH_CONTROLLER_ADDRESS="localhost:8478"; export TPU_MESH_CONTROLLER_PORT="8478"; python3 -m gunicorn -t 300 -b :5002 -w 1 -k uvicorn.workers.UvicornWorker api:app'
+tmux new-session -d -s model_app3 'export MODEL_ID="yuvalkirstain/dreamlike-photoreal-2-flax"; export TPU_HOST_BOUNDS="1,1,1"; export TPU_CHIPS_PER_HOST_BOUNDS="1,2,1"; export TPU_VISIBLE_DEVICES="2"; export TPU_MESH_CONTROLLER_ADDRESS="localhost:8478"; export TPU_MESH_CONTROLLER_PORT="8478"; python3 -m gunicorn -t 300 -b :5002 -w 1 -k uvicorn.workers.UvicornWorker api:app'
 tmux new-session -d -s model_app4 'export MODEL_ID="stabilityai/stable-diffusion-2-1"; export TPU_HOST_BOUNDS="1,1,1"; export TPU_CHIPS_PER_HOST_BOUNDS="1,2,1"; export TPU_VISIBLE_DEVICES="3"; export TPU_MESH_CONTROLLER_ADDRESS="localhost:8479"; export TPU_MESH_CONTROLLER_PORT="8479"; python3 -m gunicorn -t 300 -b :5003 -w 1 -k uvicorn.workers.UvicornWorker api:app'
 
 tmux ls
